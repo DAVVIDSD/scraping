8f24deb259031e0ac3df74c73e78725430b72b4c
DAVVIDSD <david190698@gmail.com>
2021-02-08 15:04:05 -0500

    scraping mercadolibre

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
-const puppeteer = require('puppeteer');
-const xlsx = require('xlsx');
-
-(async () => {
-  const browser = await puppeteer.launch({headless: false})
-  const page = await browser.newPage()
-  await page.goto('http://www.amazon.es');
-
-
-  // Interactuar con la barra de busqueda
-  await page.type('#twotabsearchtextbox', 'libros de javascript');
-  // Hacer el click lueggo de poner el input
-  await page.click('.nav-search-submit input');
-  // Espera hasta que la pagina cargue ese componente en especifico
-  await page.waitForSelector('[data-component-type=s-search-result]')
-  // Podemos hacer screen de la page
-  // await page.screenshot({path:'prueba.jpg'})
-  // Tiempo de esperas 
-  await page.waitFor(1000)
-
-  const enlaces = await page.evaluate(() => {
-    const elements = document.querySelectorAll('[data-component-type=s-search-result] h2 a ')
-    const links = []
-    for (let element of elements) {
-      links.push(element.href) 
-    }
-    return links
-  })
-  // console.log(enlaces);
-
-
-  const books = []
-  for (let enlace of enlaces) {
-
-    await page.goto(enlace)
-    await page.waitForSelector('#productTitle')
-    
-
-   const book =  await page.evaluate(() => {
-      const tmp = {}
-      tmp.title = document.querySelector('#productTitle').innerText;
-      tmp.author = document.querySelector('.author a').innerText;
-      // tmp.price = document.querySelector('.offer-price').innerText;
-      return tmp
+const puppeteer = require("puppeteer");
+const xlsx = require("xlsx");
+const inquirer = require("inquirer");
+(() => {
+  //input
+  inquirer
+    .prompt({
+      name: "search",
+      message: "Ingresa tu busqueda",
     })
-    books.push(book)
-  }
+    .then(async (value) => {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.goto("https://www.mercadolibre.com.co/");
+      // Interactuar con la barra de busqueda
+      await page.type(".nav-search-input", `${value.search}`);
+      // Hacer el click luego de poner el input
+      await page.click(".nav-search-btn");
+      // Espera hasta que la pagina cargue ese componente en especifico
+      await page.waitForSelector(".ui-search-results");
+      //Extraer links
+      const enlaces = await page.evaluate(() => {
+        const elements = document.querySelectorAll(
+          ".ui-search-item__group--title a"
+        );
+        const links = [];
+        for (let element of elements) {
+          links.push(element.href);
+        }
+        return links;
+      });
 
-  // console.log(books)
-  const wb = xlsx.utils.book_new();
-  const ws = xlsx.utils.json_to_sheet(books)
-  xlsx.utils.book_append_sheet(wb,ws)
-  xlsx.writeFile(wb, "books.xlsx")
-  await browser.close()
-  // return books;
+      //Data final
+      const finishData = [];
+      // Recorrer cada producto
+      for (let enlace of enlaces) {
+        await page.goto(enlace);
+        await page.waitForSelector(".ui-pdp-title");
+
+        const data = await page.evaluate(() => {
+          const tmp = {};
+          tmp.title = document.querySelector(".ui-pdp-title").innerText;
+          tmp.price = document.querySelector(".price-tag-fraction").innerText;
+          tmp.description = document.querySelector(
+            ".ui-pdp-description__content"
+          ).innerText;
+          const link = document.querySelector(".ui-pdp-image ").src;
+          const replaces = link.replace("Q", "NQ");
+          const newLink = replaces.replace("R", "O");
+          tmp.img = newLink;
+          return tmp;
+        });
+        finishData.push(data);
+      }
+      // Transformar a documento
+      const wb = xlsx.utils.book_new();
+      const ws = xlsx.utils.json_to_sheet(finishData);
+      xlsx.utils.book_append_sheet(wb, ws);
+      xlsx.writeFile(wb, "data-mercadolibre.xlsx");
+      await browser.close();
+    });
 })();
-
